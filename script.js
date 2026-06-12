b340eeed5019feb8efd667f3beb36fe068f61a2d
Febin <febinsijuarackan@gmail.com>
2026-06-12 16:23:08 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Portfolio loaded successfully!");
+
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const section = document.querySelector(this.getAttribute("href"));
+
+    section.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
